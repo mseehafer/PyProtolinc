029d7a0e6cd63548fe7b11bad7dbe43de8f82fce
martin <mseehafer@gmx.de>
2022-08-03 10:28:09 +0200

    doc setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,3 +85,5 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+nbsphinx_kernel_name = 'python'
